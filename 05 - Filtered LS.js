4f20e33888a3ed1e49db84609df2e69c321a93f7
Manjunatha D <manjunathad13@gmail.com>
2017-12-29 11:35:24 +0530

    05 - Filtered LS -  minor changes

--- 05 - Filtered LS.js
+++ 05 - Filtered LS.js
@@ -4,11 +4,14 @@ const dirPath = process.argv[2];
 const fileExtension = process.argv[3];
 
 const regex = new RegExp(`\.${fileExtension}$`);
+// TODO: modify this to make sure that the file name contains at least on character.
+// something like `?*\.${fileExension}$`
+
 // returns an array of files and directories in the given directory
 fs.readdir(dirPath, (err, data) => {
   // filter files with the given file extension
   const filteredFiles = data.filter(file => {
-    return regex.test(file);
+    return regex.test(file); // The problem with this is it will match files like .txt, .md, etc i.e. files with just extension, but no valid file name.
     //file.endsWith("." + fileExtension); won't work. It returns true if the file name contains the extension, not necessarily at the end.
   });
 
